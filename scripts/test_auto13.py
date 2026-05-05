7fea0af1c4b79d399c960104371fe4bc70c471b9
Michael.Ma <malei15022182738@gmail.com>
2026-05-05 22:09:57 +0800

    script .py file update.

--- scripts/test_auto13.py
+++ scripts/test_auto13.py
@@ -1,12 +1,20 @@
 # -*- coding: utf-8 -*-
-
-import pytest
-
-@pytest.mark.func
-def test_function():
-    print(f't_0用例被运行!!!')
+def setup_function():
+    print("setup_function")
 
 
+def teardown_function():
+    print("teardown_function")
 
+def test_01():
+    print("---用例a执行---")
 
+class TestCase():
+    def test_02(self):
+        print("---用例b执行---")
 
+    def test_03(self):
+        print("---用例c执行---")
+
+def test_04():
+    print("---用例d执行---")
